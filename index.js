cd0ba6c475e8c40693d613cd2ae874a9be9f7b4a
Jessica Perez <1jessicaperez@gmail.com>
2020-06-06 01:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper(string) {
   console.log (string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.isLowerCase === true ){
+  if (string.toLowerCase === string ){
     return ("I can't hear you!")
-  } else if (string.isUpperCase === true ) {
+  } else if (string.toUpperCase === string ) {
     return ("YES INDEED!")
   } else if (string === "I love you, Grandma."){
     return ( "I love you, too.")
